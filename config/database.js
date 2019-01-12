5a55fb20ff4a5cc99c8391da5eaae59a358e17f0
Galina Pitts <galina_pitts@icloud.com>
2019-01-12 15:56:57 -0800

    third commit

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == 'production') {
-  module.exports = {mongoURI: 'mongodb://<dbuser>:<dbpassword>@ds255784.mlab.com:55784/news-scraper'}
+  module.exports = {mongoURI: 'mongodb://brianpx1:milka1998@ds255784.mlab.com:55784/news-scraper'}
   } else {
   module.exports= {mongoURI: 'mongodb://localhost/news-scraper'}
 }
